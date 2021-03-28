2263b7ba46606db7aef62970541437483f41d929
Andrei <andrei-a-c-s@hotmail.com>
2021-03-28 19:46:18 -0300

    A cada linha analisada do csv, é feito o processamento da mesma no serviço da receita. Retorno de um novo csv com resultado do envio

--- src/main/java/com/sicredi/receita/controller/ReceitaController.java
+++ src/main/java/com/sicredi/receita/controller/ReceitaController.java
@@ -1,9 +1,14 @@
 package com.sicredi.receita.controller;
 
 
+import com.sicredi.receita.dto.RespostaDTO;
 import com.sicredi.receita.service.ReceitaIntegracaoService;
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.text.ParseException;
 
@@ -22,8 +28,17 @@ public class ReceitaController {
 
     private final ReceitaIntegracaoService receitaIntegracaoService;
 
-    @PostMapping("/csv-file")
-    public ResponseEntity<?> teste(@RequestParam("file") MultipartFile csvFile) throws IOException, InterruptedException, ParseException {
-        return ResponseEntity.ok(receitaIntegracaoService.processaEnviaCsvReceita(csvFile));
+    @PostMapping(value = "/csv-file", produces = "application/csv")
+    public ResponseEntity<Resource> enviaCsv(@RequestParam("file") MultipartFile csv) throws IOException, InterruptedException, ParseException {
+        RespostaDTO<ByteArrayOutputStream> resposta = receitaIntegracaoService.processaEnviaCsvReceita(csv);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" + csv.getName());
+        httpHeaders.add(HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS, HttpHeaders.CONTENT_DISPOSITION);
+        ByteArrayResource resource = new ByteArrayResource(resposta.getData().toByteArray());
+        return ResponseEntity.ok()
+                .headers(httpHeaders)
+                .contentLength(resource.contentLength())
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(resource);
     }
 }
